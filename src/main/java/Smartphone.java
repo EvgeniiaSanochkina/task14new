e9a43177eb202ebd82bb119ad202503a33374d42
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-10-16 23:30:51 +0700

    done

--- src/main/java/Smartphone.java
+++
@@ -1,9 +0,0 @@
-public class Smartphone extends Product{
-    private String producer;
-
-
-    public Smartphone(int id, String name, int price, String producer) {
-
-        super(id, name, price);
-    }
-}
